c388cfef6b5d67c374eb1c43044ec5dd73e9d7ad
Sebastian Drost <s.drost@52north.org>
2022-06-22 15:34:27 +0200

    Add daymet file discovery for id

--- scripts/ioutils.py
+++ scripts/ioutils.py
@@ -37,7 +37,7 @@ def discover_daymet_files(data_dir: str, variables: list):
         sub_dir_name_list = os.listdir(var_dir)
         for sub_dir_name in sub_dir_name_list:
             sub_dir = os.path.join(var_dir, sub_dir_name)
-            files = glob.glob(f"{sub_dir}/{sub_dir_name}*{variable}*")
+            files = glob.glob(f"{sub_dir}/{sub_dir_name}*{variable}*.nc")
             if not files:
                 logger.warning(f"No files found in path {data_dir} for variable {variable} and sub directory"
                                f"{sub_dir_name}")
@@ -49,7 +49,7 @@ def discover_daymet_files(data_dir: str, variables: list):
     return file_dict
 
 
-def discover_daymet_files_for_id(root_data_dir: str, id: str, variables: list):
+def discover_daymet_files_for_id_and_variable(root_data_dir: str, id: str, variables: list):
     """
     Discovers all Daymet NetCDF files from a root directory for a given id and specified variables.
 
@@ -78,9 +78,43 @@ def discover_daymet_files_for_id(root_data_dir: str, id: str, variables: list):
     file_list = []
     for variable in variables:
         data_dir = os.path.join(root_data_dir, variable, id)
-        files = glob.glob(f"{data_dir}/{id}*{variable}*")
+        files = glob.glob(f"{data_dir}/{id}*{variable}*.nc")
         if not files:
             logger.warning(f"No files found in path {data_dir} for variable {variable} and basin {id}")
         else:
             file_list.extend(files)
     return file_list
+
+
+def discover_daymet_file_for_id(root_data_dir: str, id: str):
+    """
+    Discovers all Daymet NetCDF files from a root directory for a given id.
+
+    Folder structure and file naming must follow the following convention {root_dir}/{id}_daymet_v4_daily_na_*.nc.
+    E.g. for given ID 123 the method will discover within the directory {root_dir} all NetCDF files with filename
+    123_daymet_v4_daily_na_*.nc. Files and directories that do not follow these conventions will be ignored.
+
+    Parameters
+    ----------
+    root_data_dir: str
+        Root data dir
+    id: str
+        ID which will be used to discover files for the same region in the different variable folders
+        variables: Daymet variables to discover the NetCDF files for
+
+    Returns
+    -------
+    List
+        List of NetCDF file paths
+
+    """
+    files = glob.glob(f"{root_data_dir}/{id}_daymet_v4_daily_na.nc")
+    if not files or len(files) == 0:
+        raise FileNotFoundError(f"No files found in path {root_data_dir} for basin {id}")
+    if len(files) > 1:
+        logger.warning(f"More than one file was found in path {root_data_dir} for basin {id}. Only first one will be returned.")
+    return files[0]
+
+
+def simple_file_discovery(data_dir: str):
+    return os.listdir(data_dir)
